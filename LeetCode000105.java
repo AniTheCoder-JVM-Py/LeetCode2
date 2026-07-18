7f8a4cfb197f570db9f69f1cfb2bc72f6d3daf8f
AniTheCoder-JVM-Py <b25ec021@nitm.ac.in>
2026-07-18 15:37:07 +0530

    uploaded Construct Binary Tree from Inorder and Preorder Traversal

---
+++ LeetCode000105.java
@@ -0,0 +1,71 @@
+/*Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+ 
+
+Example 1:
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+
+ 
+
+Constraints:
+
+    1 <= preorder.length <= 3000
+    inorder.length == preorder.length
+    -3000 <= preorder[i], inorder[i] <= 3000
+    preorder and inorder consist of unique values.
+    Each value of inorder also appears in preorder.
+    preorder is guaranteed to be the preorder traversal of the tree.
+    inorder is guaranteed to be the inorder traversal of the tree.*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int preIndex = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+        for(int i = 0; i < inorder.length; i++)
+            map.put(inorder[i], i);
+        
+        return build(preorder, 0, inorder.length-1);
+    }
+
+    public TreeNode build(int[] preorder, int left, int right){
+
+        if(left > right)
+            return null;
+        
+        int rootVal = preorder[preIndex];
+        preIndex++;
+        TreeNode root = new TreeNode(rootVal);
+
+        int mid = map.get(rootVal);
+
+        root.left = build(preorder, left, mid-1);
+        root.right = build(preorder, mid+1, right);
+
+        return root;
+    }
+}
