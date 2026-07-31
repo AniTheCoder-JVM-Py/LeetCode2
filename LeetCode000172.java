f287c92a6daaa5bbb051fee09d617ed1815bd022
AniTheCoder-JVM-Py <b25ec021@nitm.ac.in>
2026-07-31 20:05:20 +0530

    uploaded Factorial Trailing Zeros

---
+++ LeetCode000172.java
@@ -0,0 +1,35 @@
+/*Given an integer n, return the number of trailing zeroes in n!.
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+
+Example 1:
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+
+Example 2:
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+
+Example 3:
+Input: n = 0
+Output: 0
+
+Constraints:
+    0 <= n <= 104*/
+
+class Solution {
+    public int trailingZeroes(int n) {
+        return trail(n, 0);
+    }
+
+    public int trail(int n, int count){ 
+        if(n <= 0)
+            return count;
+        else{
+            n = n/5;
+            count+=n;
+            return trail(n, count);
+        }
+    }
+}
